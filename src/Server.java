fcf3a91b23080a91b40875319722fb44d15f6d10
dobby <maximilian.knut@gmail.com>
2025-03-07 15:02:38 +0100

    server class

--- src/Server.java
+++ src/Server.java
@@ -1,5 +1,13 @@
-public class Server {
-    public Server(int port){
+import se.mau.DA343A.VT25.projekt.net.ListeningSocket;
+import se.mau.DA343A.VT25.projekt.net.ListeningSocketConnectionWorker;
 
+public class Server implements ListeningSocket {
+    public Server(int port) {
+    }
+
+
+    @Override
+    public ListeningSocketConnectionWorker createNewConnectionWorker() {
+        return null;
     }
 }
