c7cbaded490b588eb2d43eb7a7e5f3829ed13e27
Othman Al-Ani <142592326+Othman-Al-Ani@users.noreply.github.com>
2025-03-10 15:50:55 +0100

    sends info to server

--- src/Client.java
+++ src/Client.java
@@ -1,18 +1,18 @@
 import se.mau.DA343A.VT25.projekt.Buffer;
+import se.mau.DA343A.VT25.projekt.net.SecurityTokens;
 
 import javax.swing.*;
 import javax.swing.event.ChangeEvent;
 import javax.swing.event.ChangeListener;
+import java.awt.*;
 import java.beans.PropertyChangeEvent;
 import java.beans.PropertyChangeListener;
-import java.io.DataOutput;
-import java.io.DataOutputStream;
-import java.io.IOException;
+import java.io.*;
 import java.net.Socket;
 
 public class Client extends JFrame implements ChangeListener, Runnable{
 
-    private int MaxPower;
+
     private String ApplianceName;
     private String ip;
     private int port;
@@ -26,41 +26,83 @@ public class Client extends JFrame implements ChangeListener, Runnable{
         slider = new JSlider(0, MaxPower, 0);
         label = new JLabel(ApplianceName);
         frame = new JFrame();
+        frame.setLayout(new GridLayout(2,1));
         this.ip = ip;
         this.port = port;
 
+        this.ApplianceName = ApplianceName;
+
         frame.setSize(300,300);
 
-        slider.setBounds(50,30,100,100);
-//        label.setBounds(50,60,100,100);
-        frame.add(slider);
+        label.setFont(new Font("Arial", Font.BOLD, 20));
+
+        slider.setMajorTickSpacing((MaxPower/4));
+        slider.setMinorTickSpacing(1);
+        slider.setLabelTable(slider.createStandardLabels(MaxPower/4));
+        slider.setPaintTicks(true);
+        slider.setPaintLabels(true);
+
+        //slider.setBounds(50,30,100,100);
+        label.setBounds(50,60,100,100);
+
+
         frame.add(label);
+        frame.add(slider);
+
+
+
+
 
     }
 
+
     @Override
     public void run() {
-        frame.setVisible(true);
+        frame.show(true);
 //        slider.addPropertyChangeListener("value",this );
 //        slider.addChangeListener(e -> {slider.firePropertyChange("value",0,slider.getValue());});
+       SecurityTokens token = new SecurityTokens("martinsKuk");
 
         slider.addChangeListener(this);
         try {
             Socket socket = new Socket(ip,port);
 
-        } catch (IOException e) {
+            DataOutputStream out = new DataOutputStream( socket.getOutputStream());
+
+
+            out.writeUTF(token.generateToken());
+
+            out.writeUTF( ApplianceName);
+
+
+            while (true){
+
+                out.write((int) buffer.get());
+
+
+            }
+
+        }
+        catch (IOException | InterruptedException e) {
             throw new RuntimeException(e);
         }
+
     }
+
 
 //    @Override
 //    public void propertyChange(PropertyChangeEvent evt) {
 //
 //    }
 
+
+
+
     @Override
     public void stateChanged(ChangeEvent e) {
+
         buffer.put(slider.getValue());
+
 
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,14 @@
 public class Main {
     public static void main(String[] args){
-        //Client client = new Client(30, "Laptop", "localhost",42069);
-        //Thread thread = new Thread(client);
-        //thread.start();
+        Client client = new Client(15, "Laptop", "localhost",42069);
+        Thread thread = new Thread(client);
+
 
 
         Server server = new Server(42069);
+
         Thread Sthread = new Thread(server);
         Sthread.start();
+        thread.start();
     }
 }
