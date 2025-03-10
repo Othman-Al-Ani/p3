239782181ef61aa58a93c1a854c48a21fbde41a5
Othman Al-Ani <142592326+Othman-Al-Ani@users.noreply.github.com>
2025-03-10 19:58:27 +0100

    disconnects update

--- src/Client.java
+++ src/Client.java
@@ -14,49 +14,56 @@ import java.beans.PropertyChangeListener;
 import java.io.*;
 import java.net.Socket;
 
-public class Client extends JFrame implements ChangeListener, Runnable, IAppExitingCallback {
+public class Client extends JFrame implements PropertyChangeListener, Runnable, IAppExitingCallback {
 
 
-private IAppExitingCallback onExitingCallback;
+    private IAppExitingCallback onExitingCallback;
     private String ApplianceName;
     private String ip;
     private int port;
-    private Buffer buffer = new Buffer<>();
-
+    private Buffer<Integer> buffer = new Buffer<>();
+    private DataOutputStream out;
     private JFrame frame;
     private JLabel label;
     private JSlider slider;
     private Socket socket;
+
     public Client(int MaxPower, String ApplianceName, String ip, int port) throws IOException {
         slider = new JSlider(0, MaxPower, 0);
         label = new JLabel(ApplianceName);
         frame = new JFrame();
-        frame.setLayout(new GridLayout(2,1));
+        frame.setLayout(new GridLayout(2, 1));
         this.ip = ip;
         this.port = port;
 
         this.ApplianceName = ApplianceName;
 
-        frame.setSize(300,300);
+        frame.setSize(300, 300);
 
         label.setFont(new Font("Arial", Font.BOLD, 20));
 
-        slider.setMajorTickSpacing((MaxPower/4));
+        slider.setMajorTickSpacing((MaxPower / 4));
         slider.setMinorTickSpacing(1);
-        slider.setLabelTable(slider.createStandardLabels(MaxPower/4));
+        slider.setLabelTable(slider.createStandardLabels(MaxPower / 4));
         slider.setPaintTicks(true);
         slider.setPaintLabels(true);
 
         //slider.setBounds(50,30,100,100);
-        label.setBounds(50,60,100,100);
+        label.setBounds(50, 60, 100, 100);
 
 
         frame.add(label);
         frame.add(slider);
 
 
+        frame.addWindowListener(new java.awt.event.WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent e) {
 
-         socket = new Socket(ip,port);
+                exiting();
+
+            }
+        });
 
 
     }
@@ -65,70 +72,61 @@ private IAppExitingCallback onExitingCallback;
     @Override
     public void run() {
         frame.show(true);
-//        slider.addPropertyChangeListener("value",this );
-//        slider.addChangeListener(e -> {slider.firePropertyChange("value",0,slider.getValue());});
-       SecurityTokens token = new SecurityTokens("martinsKuk");
+        slider.addPropertyChangeListener("value", this);
+        slider.addChangeListener(e -> {
+            slider.firePropertyChange("value", 0, slider.getValue());
+        });
 
-        slider.addChangeListener(this);
+        // slider.addChangeListener(this);
+
+        SecurityTokens token = new SecurityTokens("martin");
+
         try {
+            socket = new Socket(ip, port);
 
-            DataOutputStream out = new DataOutputStream( socket.getOutputStream());
+            out = new DataOutputStream(socket.getOutputStream());
 
 
             out.writeUTF(token.generateToken());
 
-            out.writeUTF( ApplianceName);
+            out.writeUTF(ApplianceName);
 
 
-            while (true){
+            while (true) {
 
-                out.write((int) buffer.get());
+
+                out.write(1);
+                out.write(buffer.get());
 
 
             }
 
-        }
-        catch (IOException | InterruptedException e) {
+
+        } catch (IOException | InterruptedException e) {
             throw new RuntimeException(e);
+
         }
 
     }
-
-
-//    @Override
-//    public void propertyChange(PropertyChangeEvent evt) {
-//
-//    }
-
-
 
 
     @Override
-    public void stateChanged(ChangeEvent e) {
-
+    public void propertyChange(PropertyChangeEvent evt) {
         buffer.put(slider.getValue());
 
-
     }
 
-    protected void processWindowEvent(WindowEvent e) {
-        if (e.getID() == 201) {
-            if (this.onExitingCallback != null) {
-                onExitingCallback.exiting();
-            }
-
-            System.exit(0);
-        }
-
-        super.processWindowEvent(e);
-    }
 
     @Override
     public void exiting() {
 
         try {
+            System.out.println("closed");
+            out.write(0);
+            out.write(buffer.get());
+
             socket.close();
-        } catch (IOException e) {
+        } catch (IOException |InterruptedException e) {
             throw new RuntimeException(e);
         }
 
